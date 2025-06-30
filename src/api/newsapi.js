ca248a8aca2a14d849b1c44def4d8993392368cf
vamshibanoth <vamshibanoth3@gmail.com>
2025-06-30 11:27:26 +0530

    Updated the .env for the api keys

--- src/api/newsapi.js
+++ src/api/newsapi.js
@@ -1,5 +1,6 @@
+import { NEWS_API_KEY } from "@env";
 export const fetchArticles = async (topic = "crypto") => {
-  const url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=6a52bdc7de3c49d38895cdfba705c005`;
+  const url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${NEWS_API_KEY}`;
 
   const res = await fetch(url);
 
